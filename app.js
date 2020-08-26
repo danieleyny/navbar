a56e8c6e97fdb18031a0fc9639f97512543ddfe2
Daniel eyny <danieleyny@daniels-mbp.home>
2020-08-26 03:53:26 -0400

    final update

--- app.js
+++ app.js
@@ -2,16 +2,24 @@ const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li');
-    // TOGGLE NAV
+    
     burger.addEventListener('click', ()=> {
+        // TOGGLE NAV
         nav.classList.toggle('nav-active');
 
-    });
     //ANIMATE LINKS 
     navLinks.forEach((link, index)=>{
-            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.7}s`;
-
+        if(link.style.animation){
+            link.style.animation = '';
+        }   else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + .5}s`;
+        }
     });
 
+    //burger animation
+    burger.classList.toggle('toggle');
+
+    });
+  
 }
 navSlide();
